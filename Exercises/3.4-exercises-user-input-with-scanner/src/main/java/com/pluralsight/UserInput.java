0af8509901a00553acdfe0226da099618695bc50
Student <nguyenanthonyd4@gmail.com>
2026-01-14 22:46:44 -0800

    User input with scanner

---
+++ Exercises/3.4-exercises-user-input-with-scanner/src/main/java/com/pluralsight/UserInput.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
+
+        System.out.println("\nHello " + name + "!");
+        System.out.println("I am " + age + " years old.");
+
+        if (age >= 18) {
+            System.out.println("You are old!");
+
+        } else {
+            System.out.println("Ha you baby!");
+
+        }
+
+        scanner.close();
+    }
+}
+
