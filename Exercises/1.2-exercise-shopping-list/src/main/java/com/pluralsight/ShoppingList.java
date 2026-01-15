68b7123e891eaab355071a323f89afae01bccad3
Student <nguyenanthonyd4@gmail.com>
2026-01-14 22:26:25 -0800

    printed apricot 10 times

--- Exercises/1.2-exercise-shopping-list/src/main/java/com/pluralsight/ShoppingList.java
+++ Exercises/1.2-exercise-shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -2,8 +2,20 @@ package com.pluralsight;
 
 public class ShoppingList {
     public static void main(String[] args){
+        System.out.println("apricot");
+        System.out.println("apricot");
+        System.out.println("apricot");
+        System.out.println("apricot");
+        System.out.println("apricot");
+        System.out.println("apricot");
+        System.out.println("apricot");
+        System.out.println("apricot");
+        System.out.println("apricot");
+        System.out.println("apricot");
+    }
 }
-}
+
+
 
 
 
